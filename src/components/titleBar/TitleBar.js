f9f5472efb38450e3eae871d3c4da4c9c5e996b1
shahrukh-saeed <117412909+shahrukh-saeed@users.noreply.github.com>
2023-06-15 00:51:17 -0400

    style add flights form

--- src/components/titleBar/TitleBar.js
+++ src/components/titleBar/TitleBar.js
@@ -10,6 +10,8 @@ const TitleBar = () => {
           <img src={planeIcon} width="30" height="30" className="planeIcon d-inline-block align-top" alt=""></img>
           Airline Seating Reservation System
         </a>
+
+        <a class="nav-link" href="/">Home</a>
       </nav>
     </div>
   );
